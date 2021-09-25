7d29da9c782aeb72c209e36a19f4f48499ea527b
yoonhero <yoonhero06@naver.com>
2021-09-25 09:56:43 +0900

    #5 logistic

--- sigmoid.py
+++ sigmoid.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 data = [[2, 0], [4, 0], [6, 0], [8, 1], [10, 1], [12, 1], [14, 1]]
 
@@ -10,3 +11,32 @@ plt.xlim(0, 15)
 plt.ylim(-.1, 1.1)
 
 plt.show()
+
+a = 0
+b = 0
+
+lr = 0.05
+
+
+def sigmoid(x):
+    return 1 / (1 + np.e ** (-x))
+
+
+for i in range(2001):
+    for x_data, y_data in data:
+        a_diff = x_data*(sigmoid(a*x_data + b) - y_data)
+
+        b_diff = sigmoid(a * x_data + b) - y_data
+
+        a = a - lr * a_diff
+        b = b - lr * b_diff
+        if i % 1000 == 0:
+            print("epoch=%.f, y = %.04f x + %.04f" % (i, a, b))
+
+    plt.scatter(x_data, y_data)
+    plt.xlim(0, 15)
+    plt.ylim(-.1, 1.1)
+    x_range = (np.arange(0, 15, 0.1))
+    plt.plot(x_range, np.array([sigmoid(a * x + b) for x in x_range]))
+
+    plt.show()
